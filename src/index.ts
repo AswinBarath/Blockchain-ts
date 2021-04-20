d218d95095eff52cabcff822005aabdd17000702
AswinBarath <aswin2001barath@gmail.com>
2021-04-20 15:38:58 +0530

    Formatting and Bug fix

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,19 @@ import * as CryptoJS from "crypto-js";
 
 class Block {
     static calculateBlockHash = (
-            index: number, 
-            previousHash:string, 
-            timestamp: number,
-            data: string 
-        ): string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+        index: number,
+        previousHash: string,
+        timestamp: number,
+        data: string
+    ): string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
 
-        static validateStructure = (aBlock: Block): boolean => 
-        typeof aBlock.index === "number" && 
-        typeof aBlock.hash === "string" && 
+    static validateStructure = (aBlock: Block): boolean =>
+        typeof aBlock.index === "number" &&
+        typeof aBlock.hash === "string" &&
         typeof aBlock.previousHash === "string" &&
         typeof aBlock.data === "string";
 
-    
+
     public index: number;
     public hash: string;
     public previousHash: string;
@@ -42,51 +42,52 @@ const genesisBlock: Block = new Block(0, "20212021202120", "", "Vanakam", 123456
 
 let blockchain: Block[] = [genesisBlock];
 
-const getBlockchain = () : Block[] => blockchain;
+const getBlockchain = (): Block[] => blockchain;
 
-const getLatestBlock = () : Block => blockchain[blockchain.length - 1];
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
 
-const getNewTimestamp = () : number => Math.round(new Date().getTime() / 1000);
+const getNewTimestamp = (): number => Math.round(new Date().getTime() / 1000);
 
-const createNewBlock = (data: string) : Block => {
+const getHashforBlock = (aBlock: Block): string => Block.calculateBlockHash(aBlock.index, aBlock.previousHash, aBlock.timestamp, aBlock.data);
+
+const createNewBlock = (data: string): Block => {
     const previousBlock: Block = getLatestBlock();
     const newIndex: number = previousBlock.index + 1;
     const newTimestamp: number = getNewTimestamp();
     const newHash: string = Block.calculateBlockHash(
-        newIndex, 
-        previousBlock.hash, 
-        newTimestamp, data
-        );
-    
+        newIndex,
+        previousBlock.hash,
+        newTimestamp,
+        data
+    );
+
     const newBlock: Block = new Block(
-        newIndex, 
-        newHash, 
-        previousBlock.hash, 
-        data, 
+        newIndex,
+        newHash,
+        previousBlock.hash,
+        data,
         newTimestamp
-        );
+    );
     addBlock(newBlock);
     return newBlock;
 }
 
-const getHashforBlock = (aBlock: Block): string => Block.calculateBlockHash(aBlock.index, aBlock.previousHash, aBlock.timestamp, aBlock.data);
-
 const isBlockValid = (candidateBlock: Block, previousBlock: Block): boolean => {
-    if(!Block.validateStructure(candidateBlock)) {
+    if (!Block.validateStructure(candidateBlock)) {
         return false;
-    } else if(previousBlock.index + 1 !== candidateBlock.index) {
+    } else if (previousBlock.index + 1 !== candidateBlock.index) {
         return false;
-    } else if(previousBlock.hash !== candidateBlock.previousHash) {
+    } else if (previousBlock.hash !== candidateBlock.previousHash) {
         return false;
-    } else if(getHashforBlock(candidateBlock) !== candidateBlock.hash) {
+    } else if (getHashforBlock(candidateBlock) !== candidateBlock.hash) {
         return false;
     } else {
         return true;
     }
 };
 
-const addBlock = (candidateBlock: Block) : void => {
-    if(isBlockValid(candidateBlock, getLatestBlock())) {
+const addBlock = (candidateBlock: Block): void => {
+    if (isBlockValid(candidateBlock, getLatestBlock())) {
         blockchain.push(candidateBlock);
     }
 }
@@ -94,6 +95,6 @@ const addBlock = (candidateBlock: Block) : void => {
 createNewBlock("2nd block");
 createNewBlock("3rd block");
 createNewBlock("4th block");
-console.log(blockchain);
+console.log(getBlockchain());
 
-export {};
+export { };

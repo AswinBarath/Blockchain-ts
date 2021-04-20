58e6e049be44c8d346f9bcade9f43b81eb41805c
AswinBarath <aswin2001barath@gmail.com>
2021-04-20 08:47:14 +0530

    Adding block properties

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,25 @@
+import * as CryptoJS from "crypto-js";
+
 class Block {
     public index: number;
     public hash: string;
     public previousHash: string;
     public data: string;
     public timestamp: number;
+
+    static calculateBlockHash = (
+            index: number, 
+            previousHash:string, 
+            timestamp: number,
+            data: string 
+        ): string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
     constructor(
         index: number,
-    hash: string,
-    previousHash: string,
-    data: string,
-    timestamp: number,
+        hash: string,
+        previousHash: string,
+        data: string,
+        timestamp: number,
     ) {
         this.index = index;
         this.hash = hash;
@@ -21,7 +31,13 @@ class Block {
 
 const genesisBlock: Block = new Block(0, "20212021202120", "", "Vanakam", 123456);
 
-let blockchain: [Block] = [genesisBlock];
+let blockchain: Block[] = [genesisBlock];
+
+const getBlockchain = () : Block[] => blockchain;
+
+const getLatestBlock = () : Block => blockchain[blockchain.length - 1];
+
+const getNewTimeStamp = () : number => Math.round(new Date().getTime() / 1000);
 
 console.log(blockchain);
 
